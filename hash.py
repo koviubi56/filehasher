8fd35c1eac34907bbc40832589e92f424260287f
koviubi56 <58398849+koviubi56@users.noreply.github.com>
2022-07-18 13:21:16 +0000

    docs: 📄 2021 -> 2022
    Changing 2021 in copyright strings to 2022

--- hash.py
+++ hash.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2021  Koviubi56
+Copyright (C) 2022  Koviubi56
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
